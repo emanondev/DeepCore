aa377928ec0803902e0c744ed586226b50089063
emanon <emanon_dev@hotmail.it>
2022-11-24 05:28:45 +0100

    hexa color with format &#RRGGBB

--- src/main/java/emanondev/core/UtilsString.java
+++ src/main/java/emanondev/core/UtilsString.java
@@ -148,6 +148,16 @@ public final class UtilsString {
         // color
         if (color)
             text = ChatColor.translateAlternateColorCodes('&', text);
+        try {
+            int from = 0;
+            while (text.indexOf("&#", from) >= 0) {
+                from = text.indexOf("&#", from) + 1;
+                text = text.replace(text.substring(from - 1, from + 7),
+                        net.md_5.bungee.api.ChatColor.of(text.substring(from, from + 7)).toString());
+            }
+        } catch (Throwable ignored) {
+        }
+
 
         return text;
     }
