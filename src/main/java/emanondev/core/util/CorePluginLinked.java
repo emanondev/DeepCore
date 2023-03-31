d1cb93864d96fab84a47dac697a068325dbcbb8c
emanon <emanon_dev@hotmail.it>
2023-03-31 12:05:06 +0200

    minor changes

--- src/main/java/emanondev/core/util/CorePluginLinked.java
+++ src/main/java/emanondev/core/util/CorePluginLinked.java
@@ -1,9 +1,19 @@
 package emanondev.core.util;
 
 import emanondev.core.CorePlugin;
+import emanondev.core.message.DMessage;
+import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 public interface CorePluginLinked {
 
     @NotNull CorePlugin getPlugin();
+
+    default DMessage getDMessage(@NotNull CommandSender sender, @NotNull String path, String... placeholders){
+        return new DMessage(getPlugin(),sender).appendLang(path,placeholders);
+    }
+
+    default void sendDMessage(@NotNull CommandSender sender, @NotNull String path, String... placeholders){
+        getDMessage(sender,path,placeholders).send();
+    }
 }
