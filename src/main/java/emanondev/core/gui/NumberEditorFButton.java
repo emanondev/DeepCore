a8bdf2d1b0f8ec1f5d43df529c7d2fb1b24c732b
emanon <emanon_dev@hotmail.it>
2024-03-05 10:46:42 +0100

    + fix

--- src/main/java/emanondev/core/gui/NumberEditorFButton.java
+++ src/main/java/emanondev/core/gui/NumberEditorFButton.java
@@ -8,13 +8,13 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.util.Consumer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class NumberEditorFButton<T extends Number> extends AGuiButton {
