e334f575531b592767d51cc3118c401b49fd97d2
emanon <emanon_dev@hotmail.it>
2024-03-15 09:47:07 +0100

    + Fawe generated support file are correctly deleted

--- src/main/java/emanondev/core/util/FAWECleaner.java
+++ src/main/java/emanondev/core/util/FAWECleaner.java
@@ -3,15 +3,20 @@ package emanondev.core.util;
 import com.fastasyncworldedit.core.extent.clipboard.DiskOptimizedClipboard;
 import com.sk89q.worldedit.extent.clipboard.BlockArrayClipboard;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
+import emanondev.core.CoreMain;
+import emanondev.core.YMLConfig;
 
 import java.io.File;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class FAWECleaner {
 
-    private static final HashSet<Path> paths = new HashSet<>();
+    //private static final HashSet<Path> paths = new HashSet<>();
+
+    private static final YMLConfig config = new YMLConfig(CoreMain.get(), "FAWE.yml");
 
     static void clean(Clipboard clip) {
         if (clip instanceof BlockArrayClipboard bClip)
@@ -29,26 +34,32 @@ public class FAWECleaner {
     }
 
     public static void cleanAll() {
-        for (Path path : paths) {
+        List<String> newList = new ArrayList<>();
+        for (String rawPath : config.loadStringList("to_remove", Collections.emptyList())) {
             try {
-                File file = path.toFile();
+                File file = new File(rawPath);
                 if (file.isFile())
-                    Files.delete(path);
+                    Files.delete(file.toPath());
             } catch (Throwable e) {
                 e.printStackTrace();
+                newList.add(rawPath);
             }
         }
+        config.set("to_remove", newList);
     }
 
     static void track(Clipboard clip) {
         if (clip instanceof BlockArrayClipboard bClip)
             clip = bClip.getParent();
         if (clip instanceof DiskOptimizedClipboard dClip) {
+            File file = dClip.getFile();
             try {
-                File file = dClip.getFile();
-                if (file != null)
-                    paths.add(file.toPath());
-            } catch (Exception e) {
+                if (file != null) {
+                    ArrayList<String> list = new ArrayList<>(config.loadStringList("to_remove", Collections.emptyList()));
+                    list.add(file.getAbsolutePath());
+                    config.set("to_remove", list);
+                }
+            } catch (Throwable e) {
                 e.printStackTrace();
             }
         }
